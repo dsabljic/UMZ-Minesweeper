2eef1500dd305e4465def40a08e363f60cf9d14e
dsabljic <domagoj.sabljicc@gmail.com>
2024-01-24 09:50:23 +0100

    Dodan folder s rjesenjem

---
+++ minesweeper/minesweeper.py
@@ -0,0 +1,169 @@
+import itertools
+import random
+
+class Minesweeper():
+    def __init__(self, height=8, width=8, mines=8):
+
+        # Set initial width, height, and number of mines
+        self.height = height
+        self.width = width
+        self.mines = set()
+
+        # Initialize an empty field with no mines
+        self.board = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                row.append(False)
+            self.board.append(row)
+
+        # Add mines randomly
+        while len(self.mines) != mines:
+            i = random.randrange(height)
+            j = random.randrange(width)
+            if not self.board[i][j]:
+                self.mines.add((i, j))
+                self.board[i][j] = True
+
+        # At first, player has found no mines
+        self.mines_found = set()
+
+    def print(self):
+        for i in range(self.height):
+            print("--" * self.width + "-")
+            for j in range(self.width):
+                if self.board[i][j]:
+                    print("|X", end="")
+                else:
+                    print("| ", end="")
+            print("|")
+        print("--" * self.width + "-")
+
+    def is_mine(self, cell):
+        i, j = cell
+        return self.board[i][j]
+
+    def nearby_mines(self, cell):
+        count = 0
+
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+
+                if (i, j) == cell:
+                    continue
+
+                if 0 <= i < self.height and 0 <= j < self.width:
+                    if self.board[i][j]:
+                        count += 1
+
+        return count
+
+    def won(self):
+        return self.mines_found == self.mines
+
+
+class Sentence():
+
+    def __init__(self, cells, count):
+        self.cells = set(cells)
+        self.count = count
+
+    def __eq__(self, other):
+        return self.cells == other.cells and self.count == other.count
+
+    def __str__(self):
+        return f"{self.cells} = {self.count}"
+
+    def known_mines(self):
+        if len(self.cells) == self.count and self.count != 0:
+            return self.cells
+        return set()
+
+
+    def known_safes(self):
+        if self.count == 0:
+            return self.cells
+        return set()
+
+    def mark_mine(self, cell):
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
+
+    def mark_safe(self, cell):
+        if cell in self.cells:
+            self.cells.remove(cell)
+
+
+class MinesweeperAI():
+
+    def __init__(self, height=8, width=8):
+
+        self.height = height
+        self.width = width
+
+        self.moves_made = set()
+
+        self.mines = set()
+        self.safes = set()
+
+        self.knowledge = []
+
+    def mark_mine(self, cell):
+        self.mines.add(cell)
+        for sentence in self.knowledge:
+            sentence.mark_mine(cell)
+
+    def mark_safe(self, cell):
+        self.safes.add(cell)
+        for sentence in self.knowledge:
+            sentence.mark_safe(cell)
+
+    def add_knowledge(self, cell, count):
+        self.moves_made.add(cell)
+        self.mark_safe(cell)
+
+        undeterminedCells = []
+        countMines = 0
+
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+                if (i, j) in self.mines:
+                    countMines += 1
+                if 0 <= i < self.height and 0 <= j < self.width and (i, j) not in self.safes and (i, j) not in self.mines:
+                    undeterminedCells.append((i, j))
+
+        newSentence = Sentence(undeterminedCells, count - countMines)
+        self.knowledge.append(newSentence)
+
+        for sentence in self.knowledge:
+            if sentence.known_mines():
+                for cell in sentence.known_mines().copy():
+                    self.mark_mine(cell)
+            if sentence.known_safes():
+                for cell in sentence.known_safes().copy():
+                    self.mark_safe(cell)
+
+        for sentence in self.knowledge:
+            if newSentence.cells.issubset(sentence.cells) and sentence.count > 0 and newSentence.count > 0 and newSentence != sentence:
+                newSubset = sentence.cells.difference(newSentence.cells)
+                newSentenceSubset = Sentence(list(newSubset), sentence.count - newSentence.count)
+                self.knowledge.append(newSentenceSubset)
+
+    def make_safe_move(self):
+        for cell in self.safes:
+            if cell not in self.moves_made:
+                return cell
+        return None
+
+    def make_random_move(self):
+        possibleMoves = []
+        for i in range(self.height):
+            for j in range(self.width):
+                if (i, j) not in self.moves_made and (i, j) not in self.mines:
+                    possibleMoves.append((i, j))
+
+        if len(possibleMoves):
+            return random.choice(possibleMoves)
+        else:
+            return None
